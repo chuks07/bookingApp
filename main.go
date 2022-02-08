4c333140d5c7867d3f73a2744f555d3a1a2c9435
chuksumenwa@gmail.com <chuksumenwa@gmail.com>
2022-02-08 01:23:04 +0100

    BookingAppLoop

--- main.go
+++ main.go
@@ -5,11 +5,42 @@ import "fmt"
 func main() {
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+	var bookings [] string 
+
 
 	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	
+	for {
+		// ask user for input
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
+
+		fmt.Println("Enter your firstname:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your lastname:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
+
+		fmt.Println("How any ticket(s) do you want:")
+		fmt.Scan(&userTicket)
+
+		remainingTickets= remainingTickets- userTicket
+		bookings= append(bookings, firstName + " "+ lastName)
+		
+		
+		
+		fmt.Printf("Thank you %v %v for buying %v tickets, you will receive a confirmation email at %v\n", firstName, lastName, userTicket, email)
+		fmt.Printf("Thank you for your purchase there are/is %v left\n", remainingTickets)
+		fmt.Printf("These are all the bookings: %v", bookings)
+
+	}
 }
